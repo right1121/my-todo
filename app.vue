38e779f061d6834514602cef129e25dea51e0e39
MsakiHoshino <right1121@hotmail.co.jp>
2023-03-03 04:27:22 +0900

    todo仮

--- app.vue
+++ app.vue
@@ -1,16 +1,42 @@
 <script setup lang="ts">
-interface todo {
+interface todoType {
   id: number
   title: string
 }
-type todosType = todo[]
+type todosType = todoType[]
 
-const todos: todosType = []
+const todos = ref<todosType>([])
+
+const title = ref('')
+const titleReset = () => { title.value = '' }
+
+const addTodo = () => {
+  todos.value.push({
+    id: 0,
+    title: title.value
+  })
+  titleReset()
+}
 </script>
 <template>
   <div>
-    <div v-for="todo in todos">
-      {{ todo }}
+    <div
+      v-for="todo in todos"
+      :key="todo.id"
+    >
+      {{ todo.id }}
+      {{ todo.title }}
+      分割
+      ▼
+    </div>
+    <div>
+      <input
+        v-model="title"
+        type="text"
+      >
+      <button @click="addTodo">
+        登録
+      </button>
     </div>
   </div>
 </template>
