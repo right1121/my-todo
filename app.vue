db1c42362d1452f79e265edfb620cba3c698b496
MsakiHoshino <right1121@hotmail.co.jp>
2023-03-06 00:51:01 +0900

    page作成

--- app.vue
+++ app.vue
@@ -1,42 +1,8 @@
 <script setup lang="ts">
-interface todoType {
-  id: number
-  title: string
-}
-type todosType = todoType[]
-
-const todos = ref<todosType>([])
-
-const title = ref('')
-const titleReset = () => { title.value = '' }
-
-const addTodo = () => {
-  todos.value.push({
-    id: 0,
-    title: title.value
-  })
-  titleReset()
-}
 </script>
+
 <template>
   <div>
-    <div
-      v-for="todo in todos"
-      :key="todo.id"
-    >
-      {{ todo.id }}
-      {{ todo.title }}
-      分割
-      ▼
-    </div>
-    <div>
-      <input
-        v-model="title"
-        type="text"
-      >
-      <button @click="addTodo">
-        登録
-      </button>
-    </div>
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+interface todoType {
+  id: number
+  title: string
+}
+type todosType = todoType[]
+
+const todos = ref<todosType>([])
+
+const title = ref('')
+const titleReset = () => { title.value = '' }
+
+const addTodo = () => {
+  todos.value.push({
+    id: 0,
+    title: title.value
+  })
+  titleReset()
+}
+</script>
+
+<template>
+  <div
+    v-for="todo in todos"
+    :key="todo.id"
+  >
+    {{ todo.id }}
+    {{ todo.title }}
+    分割
+    ▼
+  </div>
+  <div>
+    <input
+      v-model="title"
+      type="text"
+    >
+    <button @click="addTodo">
+      登録
+    </button>
+  </div>
+</template>
