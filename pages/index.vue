8e0b381d8614379accc847d81edb41803649b512
MsakiHoshino <right1121@hotmail.co.jp>
2023-03-06 23:58:49 +0900

    ページ階層作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,15 @@
 <script setup lang="ts">
-import Item from '~~/components/task/Item.vue'
-import { TodosType } from '~~/components/task/types'
-
-const todos = ref<TodosType>([])
-
-const title = ref('')
-const titleReset = () => { title.value = '' }
-
-const id = ref(0)
-
-const addTodo = () => {
-  todos.value.push({
-    id: id.value,
-    title: title.value,
-    status: '未着手'
-  })
-  id.value = id.value + 1
-  titleReset()
-}
-
-const taskDone = (id: number) => {
-  const todo = todos.value.find((todo) => todo.id === id)
-  if (todo?.status === undefined) return
-  todo.status = '完了'
-}
-const beginTask = (id: number) => {
-  const todo = todos.value.find((todo) => todo.id === id)
-  if (todo?.status === undefined) return
-  todo.status = '進行中'
-}
 </script>
 
 <template>
-  <div class="space-y-4">
-    <div class="flex space-x-2">
-      <input v-model="title" type="text" class="w-full input input-bordered">
-      <button @click="addTodo" class="btn btn-primary px-5">
-        登録
-      </button>
-    </div>
-    <div>
-      <Item
-        v-for="todo in todos"
-        :todo="todo"
-        :key="todo.id"
-        @click:done="taskDone"
-        @click:begin-task="beginTask"
-      />
-    </div>
+  <div>
+    <h2>プロジェクト一覧</h2>
+    <ul>
+      <li>
+        <NuxtLink :to="{ path: '/projects/d58c3951-7ad4-4c87-8044-afd0428aebc7'}">
+          プロジェクト名
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
---
+++ pages/projects/[project-id]/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h2>要件一覧</h2>
+    <div>
+      <NuxtLink :to="{ name: 'index' }">
+        TOPへ
+      </NuxtLink>
+      <NuxtLink :to="{ name: 'projects-projectid-tasks' }">
+        タスク一覧
+      </NuxtLink>
+    </div>
+  </div>
+</template>
